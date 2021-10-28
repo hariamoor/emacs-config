ea52cd1412e81ed534d416bdf51d65b4b6945e59
hariamoor <hari.amoor@droitfintech.com>
2021-10-28 16:01:30 -0400

    remove custom vars

--- init.el
+++ init.el
@@ -22,16 +22,3 @@
 
 (load "/Users/hariamoor/atl/atl-lsp/emacs/atl-mode.el")
 (add-to-list 'auto-mode-alist '("\\.atl\\'" . atl-mode))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("6b5c518d1c250a8ce17463b7e435e9e20faa84f3f7defba8b579d4f5925f60c1" default)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
